e6827abbf05186193ff3dd6f0cc7b4dd974161d8
DomenicoScala <dome.scala12@gmail.com>
2024-01-18 10:18:41 +0100

    FINE ESERCIZIO

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+    /* 1) Importazione del componente */
+    import AppTitle from './components/AppTitle.vue';
+    
+    export default {
+        data() {
+            return { count: 0 }
+        },
+        /* 2) Dichiarazione del Componente */
+        components:{
+            AppTitle
+        }
+    }
+</script>
+
+<template>
+    <!-- 3) UTILIZZO DEL COMPONENTE -->
+    <AppTitle/>
+        
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        data() {
+            return { count: 0 }
+        },
+    }
+</script>
+
+<template>
+        <div>
+            <h1>
+                La mia prima App con Vite
+            </h1>
+        </div>
+</template>
+
+<style scoped>
+
+</style>
